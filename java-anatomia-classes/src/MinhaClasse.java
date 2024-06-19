438fdea5f2f2aaecc30bf24f9501c5bc77d848ff
Kauan <kauan18rodrigues@outlook.com>
2024-06-19 16:27:15 -0300

    adaptação

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,15 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("Olá turma, sejam bem vindos");
+
+        String primeiroNome = "Kauan";
+        String segundoNome = "Rodrigues";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
 
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
